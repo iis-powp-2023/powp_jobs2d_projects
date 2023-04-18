e8bf21bb1fe852512177aa21740df85f6edfb5e6
Bartoszhumel <79085432+Bartoszhumel@users.noreply.github.com>
2023-04-18 13:43:02 +0200

    Dodanie prostego drivera logger+line

--- src/main/java/edu/kis/powp/jobs2d/drivers/DriverComposite.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/DriverComposite.java
@@ -1,5 +1,6 @@
 package edu.kis.powp.jobs2d.drivers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
@@ -7,8 +8,8 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 public class DriverComposite implements Job2dDriver {
 
     private List<Job2dDriver> drivers;
-    DriverComposite(List<Job2dDriver> drivers) {
-        this.drivers = drivers;
+    public DriverComposite() {
+        drivers = new ArrayList<Job2dDriver>();
     }
     public void addDriver(Job2dDriver driver) {
         drivers.add(driver);
@@ -22,7 +23,6 @@ public class DriverComposite implements Job2dDriver {
             driver.setPosition(x, y);
         }
     }
-
     @Override
     public void operateTo(int x, int y) {
         for(Job2dDriver driver : drivers) {
