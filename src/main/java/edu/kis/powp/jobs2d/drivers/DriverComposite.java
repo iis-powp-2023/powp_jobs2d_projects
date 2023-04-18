a975b0ad4a9ddd6456c5538fcec5913b11b4442e
Bartoszhumel <79085432+Bartoszhumel@users.noreply.github.com>
2023-04-18 13:26:49 +0200

    dodanie zawartosci do composite

--- src/main/java/edu/kis/powp/jobs2d/drivers/DriverComposite.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/DriverComposite.java
@@ -7,17 +7,27 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 public class DriverComposite implements Job2dDriver {
 
     private List<Job2dDriver> drivers;
-
+    DriverComposite(List<Job2dDriver> drivers) {
+        this.drivers = drivers;
+    }
+    public void addDriver(Job2dDriver driver) {
+        drivers.add(driver);
+    }
+    public void clearDrivers() {
+        drivers.clear();
+    }
     @Override
     public void setPosition(int x, int y) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setPosition'");
+        for (Job2dDriver driver : drivers) {
+            driver.setPosition(x, y);
+        }
     }
 
     @Override
     public void operateTo(int x, int y) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'operateTo'");
+        for(Job2dDriver driver : drivers) {
+            driver.operateTo(x, y);
+        }
     }
 
 }
