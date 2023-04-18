b9e07499540f7e38062e607da4f116442bc6af37
Wanirur <235861@edu.p.lodz.pl>
2023-04-18 13:47:31 +0200

    add clone method to ICompoundCommand

--- src/main/java/edu/kis/powp/jobs2d/command/ICompoundCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/ICompoundCommand.java
@@ -1,12 +1,14 @@
 package edu.kis.powp.jobs2d.command;
 
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Interface extending Job2dDriverCommand to execute more than one command.
  */
-public interface ICompoundCommand extends DriverCommand {
-
-	public Iterator<DriverCommand> iterator();
-
+public interface ICompoundCommand extends DriverCommand, Cloneable {
+    public Iterator<DriverCommand> iterator();
+    public void setDriverCommands(List<DriverCommand> driver);
+    public Object clone() throws CloneNotSupportedException;
+    public List<DriverCommand> getDriverCommands();
 }
