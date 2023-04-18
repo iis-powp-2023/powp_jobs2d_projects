9310658d707db6a06d5ca42133ee136672cc95f6
Martyna <235887@edu.p.lodz.pl>
2023-04-18 13:43:19 +0200

    Adding CommandVisitorCommander Class

---
+++ src/main/java/edu/kis/powp/jobs2d/command/CommandVisitorCommander.java
@@ -0,0 +1,18 @@
+package edu.kis.powp.jobs2d.command;
+
+public class CommandVisitorCommander implements CommandVisitor{
+    @Override
+    public void visitICompoundCommand(ICompoundCommand driverCommand) {
+
+    }
+
+    @Override
+    public void visitOperateToCommand(OperateToCommand operateToCommand) {
+
+    }
+
+    @Override
+    public void visitSetPositionCommand(SetPositionCommand setPositionCommand) {
+
+    }
+}
