3fe93a10a02272b5d0c944b92aaa09d03ec043cb
MateuszAdamski <235823@edu.p.lodz.pl>
2023-04-27 23:02:35 +0200

    fix iterator

--- src/main/java/edu/kis/powp/jobs2d/command/CommandVisitor.java
+++ src/main/java/edu/kis/powp/jobs2d/command/CommandVisitor.java
@@ -1,14 +1,18 @@
 package edu.kis.powp.jobs2d.command;
 
+import java.util.Iterator;
+
 public class CommandVisitor implements ICommandVisitor {
 
 	@Override
 	public void visit(ICompoundCommand command) {
 		int commandsCount = 0;
+
+		Iterator<DriverCommand> iter = command.iterator();
 		
-		while(command.iterator().hasNext()) {
+		while(iter.hasNext()) {
 			commandsCount++;
-			command.iterator().next();
+			iter.next().accept(this);
 		}
 		
 		System.out.println("Compound commands count = " + commandsCount);
