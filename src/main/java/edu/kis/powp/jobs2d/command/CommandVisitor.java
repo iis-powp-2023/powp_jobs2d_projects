52643dc394e169e9905a9962f46b7ca9f5def7a7
Barstok <mrbarstok@gmail.com>
2023-04-27 19:32:01 +0200

    ICommandVisitor implementation

---
+++ src/main/java/edu/kis/powp/jobs2d/command/CommandVisitor.java
@@ -0,0 +1,20 @@
+package edu.kis.powp.jobs2d.command;
+
+public class CommandVisitor implements ICommandVisitor {
+
+	@Override
+	public void visit(ICompoundCommand command) {
+		System.out.println("Displaying ICompoundCommand");
+	}
+
+	@Override
+	public void visit(OperateToCommand command) {
+		System.out.println("Displaying OperateToCommand");
+	}
+
+	@Override
+	public void visit(SetPositionCommand command) {
+		System.out.println("Displaying SetPositionCommand");
+	}
+	
+}
