28248ff61bc98c0a09a48c65b7bd0441191db9d9
dan525555 <77828669+dan525555@users.noreply.github.com>
2023-05-07 01:10:41 +0200

    Add Observer for HeadUsage

--- src/main/java/edu/kis/powp/jobs2d/command/manager/LoggerCommandChangeObserver.java
+++ src/main/java/edu/kis/powp/jobs2d/command/manager/LoggerCommandChangeObserver.java
@@ -1,25 +1,23 @@
 package edu.kis.powp.jobs2d.command.manager;
-
 import java.util.logging.Logger;
-
 import edu.kis.powp.jobs2d.command.DriverCommand;
 import edu.kis.powp.jobs2d.features.CommandsFeature;
-import edu.kis.powp.jobs2d.features.InkUsage.InkUsageFeature;
-import edu.kis.powp.jobs2d.features.InkUsage.InkUsageFeatureManager;
+
+
 import edu.kis.powp.observer.Subscriber;
+
 
 public class LoggerCommandChangeObserver implements Subscriber {
 
     private final static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
     public void update() {
-        InkUsageFeature inkUsageFeature = InkUsageFeatureManager.getInkUsageFeature();
+
         DriverCommand command = CommandsFeature.getDriverCommandManager().getCurrentCommand();
-        logger.info("Head distance: "+Double.toString(inkUsageFeature.getHeadDistance())+" units");
-        logger.info("Op.  distance: "+Double.toString(inkUsageFeature.getOperationDistance())+" units");
+
         logger.info("Current command set to: " + command.toString());
 
-     }
+    }
 
 
 
