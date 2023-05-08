229ec5ff07173a15df6b199a36e5dbd890054f55
Mateusz Pruss <235953@edu.p.lodz.pl>
2023-05-08 22:36:02 +0200

    feat: showing distances by logger
    Co-authored-by: Aneta Kilar <235892@edu.p.lodz.pl>

--- src/main/java/edu/kis/powp/jobs2d/features/DistanceFeature.java
+++ src/main/java/edu/kis/powp/jobs2d/features/DistanceFeature.java
@@ -1,11 +1,23 @@
 package edu.kis.powp.jobs2d.features;
 
+import edu.kis.powp.jobs2d.command.DriverCommand;
+import edu.kis.powp.jobs2d.command.manager.LoggerCommandChangeObserver;
+
+import java.util.logging.Logger;
+
 public class DistanceFeature {
     private double headDistance;
     private double operatingDistance;
 
     private int xLastPosition = 0;
     private int yLastPosition = 0;
+
+    private final static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+    private void update(){
+        logger.info("HeadDistance: " + this.headDistance);
+        logger.info("OperatingDistance: " + this.operatingDistance);
+    }
 
     private double calculateDistance(int x, int y){
         double distance = Math.sqrt(Math.pow(y - yLastPosition, 2) + Math.pow(x - xLastPosition, 2));
@@ -16,12 +28,14 @@ public class DistanceFeature {
 
     public void calculateMovingDistance(int x, int y){
         headDistance += calculateDistance(x, y);
+        update();
     }
 
     public void calculateOperatingDistance(int x, int y){
         double distance = calculateDistance(x, y);
         headDistance += distance;
         operatingDistance += distance;
+        update();
     }
 
     public double getHeadDistance() {
