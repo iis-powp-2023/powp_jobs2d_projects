6ad6f28188c905a889cc6221bac3391dcb14ad69
Alt-az <235979@edu.p.lodz.pl>
2023-05-09 10:18:09 +0200

    Immutable complex command creation

---
+++ src/main/java/edu/kis/powp/jobs2d/command/ImmutableComplexCommand.java
@@ -0,0 +1,21 @@
+package edu.kis.powp.jobs2d.command;
+
+import java.util.Iterator;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class ImmutableComplexCommand implements ICompoundCommand {
+
+    @Override
+    public void execute(Job2dDriver driver) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+
+    @Override
+    public Iterator<DriverCommand> iterator() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iterator'");
+    }
+
+}
