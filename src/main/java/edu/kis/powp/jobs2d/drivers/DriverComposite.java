0edb7efbb62fe10b37d911b7aa6a4eba34296f70
cpprian <cpprian456@gmail.com>
2023-05-10 21:16:20 +0200

    provide HeadUsageFeature as a new Job2Driver

--- src/main/java/edu/kis/powp/jobs2d/drivers/DriverComposite.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/DriverComposite.java
@@ -4,16 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
-import edu.kis.powp.jobs2d.features.HeadUsage.HeadUsageFeature;
-import edu.kis.powp.jobs2d.features.HeadUsage.HeadUsageSingleton;
 
 public class DriverComposite implements Job2dDriver {
 
     private final List<Job2dDriver> drivers;
-    private final HeadUsageFeature headUsageFeature;
     public DriverComposite() {
         drivers = new ArrayList<>();
-        headUsageFeature = HeadUsageSingleton.setupHeadUsageFeature(500.0);
     }
     public void addDriver(Job2dDriver driver) {
         drivers.add(driver);
@@ -24,16 +20,12 @@ public class DriverComposite implements Job2dDriver {
     @Override
     public void setPosition(int x, int y) {
         for (Job2dDriver driver : drivers) {
-            headUsageFeature.saveCoordinates(x, y);
             driver.setPosition(x, y);
         }
     }
     @Override
     public void operateTo(int x, int y) {
         for(Job2dDriver driver : drivers) {
-            if (!headUsageFeature.checkTonerLevel(x, y)) {
-                continue;
-            }
             driver.operateTo(x, y);
         }
     }
