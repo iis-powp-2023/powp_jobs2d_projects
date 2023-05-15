2bdaa97c54901a44308622f92557922944010fa8
Mikołaj Rajczyk <mikolajrajczyk01@gmail.com>
2023-05-15 11:28:16 +0200

    Added driver that increases size of output size by a factor of 2

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/decorator/DoubleScaleDriver.java
@@ -0,0 +1,24 @@
+package edu.kis.powp.jobs2d.drivers.decorator;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class DoubleScaleDriver extends DriverDecorator {
+    public DoubleScaleDriver(Job2dDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    public void setPosition(int x, int y) {
+        super.setPosition(x * 2,y * 2);
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+        super.operateTo(x * 2, y * 2);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
