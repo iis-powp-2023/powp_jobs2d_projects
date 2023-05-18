ff9c1c154461f430857f295b46a7a9214cf027c4
dan525555 <77828669+dan525555@users.noreply.github.com>
2023-05-18 11:11:54 +0200

    Use Isp

--- src/main/java/edu/kis/powp/jobs2d/command/manager/LoggerHeadUsageObserver.java
+++ src/main/java/edu/kis/powp/jobs2d/command/manager/LoggerHeadUsageObserver.java
@@ -2,6 +2,7 @@ package edu.kis.powp.jobs2d.command.manager;
 
 import edu.kis.powp.jobs2d.features.HeadUsage.HeadUsageManager;
 import edu.kis.powp.jobs2d.features.HeadUsage.HeadUsageFeature;
+import edu.kis.powp.jobs2d.features.HeadUsage.HeadUsageStats;
 import edu.kis.powp.observer.Subscriber;
 
 import java.util.logging.Logger;
@@ -9,15 +10,15 @@ import java.util.logging.Logger;
 public class LoggerHeadUsageObserver implements Subscriber {
 
     private final static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-    private final HeadUsageManager headUsageManager;
+    private final HeadUsageStats headUsageStats;
 
-    public LoggerHeadUsageObserver(HeadUsageManager headUsageManager) {
-        this.headUsageManager = headUsageManager;
+    public LoggerHeadUsageObserver(HeadUsageStats headUsageStats) {
+        this.headUsageStats = headUsageStats;
     }
 
     public void update() {
-        logger.info("Head distance: " + headUsageManager.getHeadDistance() + " units");
-        logger.info("Op.  distance: " + headUsageManager.getOperationDistance() + " units");
+        logger.info("Head distance: " + headUsageStats.getHeadDistance() + " units");
+        logger.info("Op.  distance: " + headUsageStats.getOperationDistance() + " units");
     }
 
     public String toString() {
