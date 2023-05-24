22a7aee05860017f22e9e5a3215ea4651f966c8b
ThePuccaPl <thepuccapl@gmail.com>
2023-05-24 12:50:03 +0200

    added factory for generating test command

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestImmutableComplexCommand.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestImmutableComplexCommand.java
@@ -1,9 +1,6 @@
 package edu.kis.powp.jobs2d.events;
 
-import edu.kis.powp.jobs2d.command.ImmutableComplexCommand;
-import edu.kis.powp.jobs2d.command.DriverCommand;
-import edu.kis.powp.jobs2d.command.OperateToCommand;
-import edu.kis.powp.jobs2d.command.SetPositionCommand;
+import edu.kis.powp.jobs2d.command.*;
 import edu.kis.powp.jobs2d.command.manager.CommandManager;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.features.CommandsFeature;
@@ -24,26 +21,7 @@ public class SelectTestImmutableComplexCommand implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        List<DriverCommand> commands = new ArrayList<>();
-        commands.add(new SetPositionCommand(3, 120));
-        commands.add(new OperateToCommand(3, 120));
-        commands.add(new OperateToCommand(120, -122));
-        commands.add(new OperateToCommand(-99, 10));
-        commands.add(new OperateToCommand(135, 60));
-        commands.add(new OperateToCommand(-34, -149));
-        commands.add(new OperateToCommand(3, 120));
-
-        List<DriverCommand> commands1 = new ArrayList<>();
-        commands1.add(new SetPositionCommand(0,0));
-        commands1.add(new OperateToCommand(50,50));
-
-        ImmutableComplexCommand nestedImmutableComplexCommand = new ImmutableComplexCommand(commands.iterator());
-
-        commands1.add(nestedImmutableComplexCommand);
-
-        ImmutableComplexCommand immutableComplexCommand = new ImmutableComplexCommand(commands1.iterator());
-
-        commands.add(new OperateToCommand(500, 500));
+        ImmutableComplexCommand immutableComplexCommand = ComplexCommandFactory.nestedShape();
 
         CommandManager manager = CommandsFeature.getDriverCommandManager();
         manager.setCurrentCommand(immutableComplexCommand);
