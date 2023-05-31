2268ad7bf1bafcdade2597ef66483d02ee61867b
WPapuga <79088499+WPapuga@users.noreply.github.com>
2023-05-31 22:46:58 +0200

    add list of currently used commands

--- src/main/java/edu/kis/powp/jobs2d/events/SelectClearPanelOptionListener.java
+++ src/main/java/edu/kis/powp/jobs2d/events/SelectClearPanelOptionListener.java
@@ -4,10 +4,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import edu.kis.powp.jobs2d.features.DrawerFeature;
+import edu.kis.powp.jobs2d.features.RecordFeature;
 
 public class SelectClearPanelOptionListener implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
+        RecordFeature.clearTransformRecording();
         DrawerFeature.getDrawerController().clearPanel();
     }
 }
