a0247ba9310a1ed65bf6533368bbf92a43ce3e22
Aneta Kilar <235892@edu.p.lodz.pl>
2023-06-01 17:22:45 +0200

    fixed usage window

--- src/main/java/edu/kis/powp/jobs2d/command/gui/UsageManagerWindow.java
+++ src/main/java/edu/kis/powp/jobs2d/command/gui/UsageManagerWindow.java
@@ -22,65 +22,61 @@ public class UsageManagerWindow extends JFrame implements WindowComponent {
         c.anchor = GridBagConstraints.LINE_START;
         content.setLayout(new GridBagLayout());
 
-        c.insets = new Insets(2, 2, 2, 2);
+        c.insets = new Insets(3, 3, 3, 3);
 
         c.gridy = 0;
         c.gridx = 0;
-        content.add(new JLabel("Usage"), c);
+        content.add(new JLabel("Usage:"), c);
 
-        c.gridy = 1;
-        c.gridx = 0;
+        c.gridx = 1;
         c.gridwidth = 3;
+        c.fill = GridBagConstraints.HORIZONTAL;
         usageBar = new JProgressBar(0, 100);
         usageBar.setValue(100);
         content.add(usageBar, c);
-
-        c.gridx = 3;
-        usageLabel = new JLabel("%");
-        content.add(usageLabel, c);
 
         c.gridy = 2;
         c.gridx = 0;
         content.add(new JLabel("Head Distance:"), c);
 
-        c.gridx = 2;
-        c.gridwidth = 3;
+        c.gridx = 1;
+        c.gridwidth = 1;
         c.fill = GridBagConstraints.HORIZONTAL;
         headDistanceField = new JTextField("0");
         headDistanceField.setColumns(10);
+        headDistanceField.setEditable(false);
         content.add(headDistanceField, c);
 
         c.gridy = 3;
         c.gridx = 0;
-        c.weightx = 1;
-        JLabel opdistance = new JLabel("Operating Distance:");
-        opdistance.setSize(opdistance.getPreferredSize());
-        content.add(opdistance, c);
+        c.gridwidth = 1;
         c.weightx = 0;
+        content.add(new JLabel("Operating Distance:"), c);
 
-        c.gridx = 2;
-        c.gridwidth = 3;
+        c.gridx = 1;
+        c.gridwidth = 1;
         c.fill = GridBagConstraints.HORIZONTAL;
         operatingDistanceField = new JTextField("0");
         operatingDistanceField.setColumns(10);
+        operatingDistanceField.setEditable(false);
         content.add(operatingDistanceField, c);
 
         c.gridy = 4;
         c.gridx = 0;
-        c.gridwidth = 4;
+        c.gridwidth = 2;
         messageField = new JLabel("WARNING", SwingConstants.CENTER);
         messageField.setForeground(Color.red);
-
         //messageField.setVisible(false);
         content.add(messageField, c);
 
-        c.gridx = 4;
+        c.gridx = 2;
+        c.gridwidth = 1;
         btnRefill = new JButton("REFILL");
         btnRefill.addActionListener((ActionEvent e) -> this.refill());
         btnRefill.setEnabled(false);
         content.add(btnRefill, c);
 
-        this.add(content, BorderLayout.LINE_START);
+        this.add(content, BorderLayout.CENTER);
     }
 
     private void refill(){
