2485069715034eda21e9428d5fb357c0e9a1f069
DMisztal <dominikmisztal@onet.eu>
2023-06-12 11:54:53 +0200

    Updates to real world driver and cleanup

--- src/main/java/edu/kis/powp/jobs2d/command/manager/CommandManager.java
+++ src/main/java/edu/kis/powp/jobs2d/command/manager/CommandManager.java
@@ -1,15 +1,10 @@
 package edu.kis.powp.jobs2d.command.manager;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-import edu.kis.powp.jobs2d.Job2dDriver;
-import edu.kis.powp.jobs2d.command.CountingCommandVisitor;
 import edu.kis.powp.jobs2d.command.DriverCommand;
-import edu.kis.powp.jobs2d.command.ICommandVisitor;
-import edu.kis.powp.jobs2d.command.ICompoundCommand;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.features.DriverFeature;
 import edu.kis.powp.jobs2d.command.ImmutableCompoundCommand;
@@ -19,7 +14,7 @@ import edu.kis.powp.observer.Subscriber;
 /**
  * Driver command Manager.
  */
-public class CommandManager implements ICommandManager{
+public class CommandManager implements ICommandManager {
     private DriverCommand currentCommand = null;
     private DriverManager driverManager = DriverFeature.getDriverManager();
 
@@ -71,18 +66,21 @@ public class CommandManager implements ICommandManager{
     public Publisher getChangePublisher() {
         return changePublisher;
     }
+
     public void deleteObservers() {
         deletedObservers = new ArrayList<>(changePublisher.getSubscribers());
         changePublisher.clearObservers();
     }
+
     public void resetObservers() {
-        for(Subscriber observer : deletedObservers){
+        for (Subscriber observer : deletedObservers) {
             changePublisher.addSubscriber(observer);
         }
     }
+
     public void runCommand() {
         DrawingThread thread = new DrawingThread(currentCommand, driverManager.getCurrentDriver());
         CompletableFuture<Void> cf = CompletableFuture.runAsync(thread);
-        //currentCommand.execute(driverManager.getCurrentDriver());
+        // currentCommand.execute(driverManager.getCurrentDriver());
     }
 }
--- src/main/java/edu/kis/powp/jobs2d/drivers/decorator/RealWorldDriver.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/decorator/RealWorldDriver.java
@@ -1,10 +1,13 @@
 package edu.kis.powp.jobs2d.drivers.decorator;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
+import java.awt.geom.Point2D;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class RealWorldDriver extends DriverDecorator {
+
     private double operateToSpeed;
     private double setPositionSpeed;
     private double operateToSetDelay;
@@ -12,13 +15,16 @@ public class RealWorldDriver extends DriverDecorator {
     private int lastX = 0;
     private int lastY = 0;
     private int partsNum = 10;
+
     private enum OperationType {
         OPERATE_TO,
         SET_POSITION
     }
+
     private OperationType lastOperationType = OperationType.SET_POSITION;
 
-    public RealWorldDriver(Job2dDriver driver, double operateToSpeed, double setPositionSpeed, double operateToSetDelay, double setToOperateDelay){
+    public RealWorldDriver(Job2dDriver driver, double operateToSpeed, double setPositionSpeed, double operateToSetDelay,
+            double setToOperateDelay) {
         super(driver);
         this.operateToSpeed = operateToSpeed;
         this.setPositionSpeed = setPositionSpeed;
@@ -26,7 +32,7 @@ public class RealWorldDriver extends DriverDecorator {
         this.setToOperateDelay = setToOperateDelay;
     }
 
-    public RealWorldDriver(Job2dDriver driver, double operateToSpeed){
+    public RealWorldDriver(Job2dDriver driver, double operateToSpeed) {
         super(driver);
         this.operateToSpeed = operateToSpeed;
         this.setPositionSpeed = 3 * operateToSpeed;
@@ -36,9 +42,9 @@ public class RealWorldDriver extends DriverDecorator {
 
     @Override
     public void setPosition(int x, int y) {
-        super.setPosition(x,y);
+        super.setPosition(x, y);
         try {
-            if(lastOperationType == OperationType.OPERATE_TO){
+            if (lastOperationType == OperationType.OPERATE_TO) {
                 Thread.sleep((long) (operateToSetDelay * 1000));
             }
             lastOperationType = OperationType.SET_POSITION;
@@ -53,17 +59,15 @@ public class RealWorldDriver extends DriverDecorator {
     @Override
     public void operateTo(int x, int y) {
         try {
-            if(lastOperationType == OperationType.SET_POSITION){
+            if (lastOperationType == OperationType.SET_POSITION) {
                 Thread.sleep((long) (setToOperateDelay * 1000));
             }
             lastOperationType = OperationType.OPERATE_TO;
-            ArrayList<Double> operationData = calculateDelays(x, y);
-            Double delay = operationData.get(0);
-            for (int i = 1; i < operationData.size(); i += 2) {
-                if (i != operationData.size() - 1) {
-                    Thread.sleep(delay.intValue());
-                }
-                super.operateTo(operationData.get(i).intValue(), operationData.get(i + 1).intValue());
+            LinePartitions operationData = calculateDelays(x, y);
+            Double delay = operationData.getDelay();
+            for (Point2D.Double point : operationData) {
+                Thread.sleep(delay.intValue());
+                super.operateTo((int) point.x, (int) point.y);
             }
             lastX = x;
             lastY = y;
@@ -77,17 +81,37 @@ public class RealWorldDriver extends DriverDecorator {
         return "Diver simulating real world time";
     }
 
-    private ArrayList<Double> calculateDelays(int destinationX, int destinationY) {
-        ArrayList<Double> output = new ArrayList<>();
+    private LinePartitions calculateDelays(int destinationX, int destinationY) {
+        ArrayList<Point2D.Double> pointsList = new ArrayList<>();
         double distance = Math.sqrt(Math.pow(lastY - destinationY, 2) + Math.pow(lastX - destinationX, 2));
-        double delay = (distance/operateToSpeed)/partsNum;
-        output.add(delay);
+        double delay = (distance / operateToSpeed) / partsNum;
         for (int i = 1; i <= partsNum; i++) {
-            double x = lastX + (double) (destinationX - lastX) / partsNum * i;
-            double y = lastY + (double) (destinationY - lastY) / partsNum * i;
-            output.add(x);
-            output.add(y);
+            Point2D.Double point = new Point2D.Double();
+            point.x = lastX + (double) (destinationX - lastX) / partsNum * i;
+            point.y = lastY + (double) (destinationY - lastY) / partsNum * i;
+            pointsList.add(point);
         }
-        return output;
+
+        return new LinePartitions(delay, pointsList);
+    }
+
+    private class LinePartitions implements Iterable<Point2D.Double> {
+        private double delay;
+        private ArrayList<Point2D.Double> points;
+
+        public LinePartitions(double delay, ArrayList<Point2D.Double> points) {
+            this.delay = delay;
+            this.points = points;
+        }
+
+        public double getDelay() {
+            return delay;
+        }
+
+        @Override
+        public Iterator<Point2D.Double> iterator() {
+            return points.iterator();
+        }
+
     }
 }
